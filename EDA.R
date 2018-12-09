52b2dd4b387854d24aac07a20c6a2f18edbb4546
oheejune <45724787+oheejune@users.noreply.github.com>
2018-12-09 11:42:33 +0900

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -1,4 +1,18 @@
 library(data.table)
 
-mydata <- fread("제5회 Big Data Competition-분석용데이터-01.Pruduct.csv",
+setwd("C:/Users/Annie/Desktop/제5회 Big Data Competition-Digital Trend Analyzer")
+Product <- fread("제5회 Big Data Competition-분석용데이터-01.Pruduct.csv",
                 encoding = "UTF-8")
+Search1 <- fread("제5회 Big Data Competition-분석용데이터-02.Search1.csv",
+                 encoding = "UTF-8")
+Search2 <- fread("제5회 Big Data Competition-분석용데이터-03.Search2.csv",
+                 encoding = "UTF-8")
+Master <- fread("제5회 Big Data Competition-분석용데이터-06.Master.csv",
+                encoding = "UTF-8")
+Custom <- fread("제5회 Big Data Competition-분석용데이터-04.Custom.csv",
+                 encoding = "UTF-8")
+Session <- fread("제5회 Big Data Competition-분석용데이터-05.Session.csv",
+                 encoding = "UTF-8")
+
+table(Custom$CLNT_AGE)
+hist(Custom$CLNT_AGE, breaks = c(10,20,30,40,50,60,70,80))
